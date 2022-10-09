94a9363d89cc7c67d880e85a12a8e7cd271db454
Liz Manresa <manresa.liz@gmail.com>
2022-10-09 18:45:48 +0200

    initial commit

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("give-away-project JS imported successfully!");
+  },
+  false
+);
---
+++ utils/consts.js
@@ -0,0 +1,3 @@
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/give-away-project";
+
+module.exports = MONGO_URI;
